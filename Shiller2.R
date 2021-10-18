96b228f9b1678c68ae56adf909854f42ef4d67fa
striker99 <16529330+striker99@users.noreply.github.com>
2021-10-18 10:25:33 -0400

    Update Shiller2.R

--- Shiller2.R
+++ Shiller2.R
@@ -4,7 +4,7 @@ Shiller2 <- function(dest.dir,
     f.name <- paste0(format(Sys.Date(), "%Y%m%d_"),
                      "ie_data.xls")
     f.path <- file.path(normalizePath(dest.dir), f.name, fsep = "\")
-  
+   
     print(f.path)
 
     if (!file.exists(f.path))
